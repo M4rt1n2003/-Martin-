7be73feabdb760cc77f6f4ebd9ba2d764676d01c
M4rt1n2003 <124209387+M4rt1n2003@users.noreply.github.com>
2023-02-28 20:50:50 -0300

    Add files via upload

---
+++ Documento.js
@@ -0,0 +1,11 @@
+document.getElementById("boton_color").addEventListener("click", function() {
+    document.body.style.backgroundColor="black";
+    
+});
+
+document.getElementById("color_principal").addEventListener("click", function() {
+    document.body.style.backgroundColor="white";
+    
+});
+
+
